e699ef91f6b1163dd270b710186a532b6187988a
subhashvilla <subhash1644villa@gmail.com>
2024-08-01 13:30:18 +0530

    second

--- script.js
+++ script.js
@@ -22,10 +22,10 @@ overlay.addEventListener('click', closemodel);
 document.addEventListener('keydown', function (e) {
     console.log(e.key);
 
-    if (e.key === 'Escape') {
-        if (!modal.classList.contains('hidden')) {
-            closemodel();
-        }
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+
+        closemodel();
+
     }
 })
 
